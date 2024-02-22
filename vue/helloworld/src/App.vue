2e28e2154cf1ba01178b73be2cab77130c69e963
mb030125 <mb030125@stust.edu.tw>
2024-02-22 21:31:45 +0800

    work2

---
+++ vue/helloworld/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import {ref,reactive} from 'vue'
+
+const message =ref('helloworld')
+
+message.value = 'helloworld2'
+
+const counter = reactive({
+  helloworld: 'helloworld3'
+})
+
+</script>
+
+
+
+<template> 
+
+  <h1>helloworld</h1><br>
+
+  <p>{{ message }}</p><br>
+
+  <p>{{ counter.helloworld }}</p>
+  
+</template> 
